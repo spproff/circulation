ea5ac5def716f4c0d1faa23c9de8f3037f453222
s.panteleev <spprof@yandex.ru>
2013-07-04 11:06:41 +0400

    Добавлен конфиг БД

---
+++ protected/config/db.php
@@ -0,0 +1,13 @@
+<?php
+ return array (
+  'class' => 'CDbConnection',
+  'connectionString' => 'mysql:host=lepotart.ru;port=3306;dbname=calculation',
+  'username' => 'calculation',
+  'password' => 'apelohicavaxi059',
+  'emulatePrepare' => true,
+  'charset' => 'utf8',
+  'enableParamLogging' => 0,
+  'enableProfiling' => 0,
+  'schemaCachingDuration' => 0,
+  'tablePrefix' => 'calc_',
+);
